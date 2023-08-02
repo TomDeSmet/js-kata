32d118ab7a4052b5322d2cb7fb628972e9756a0f
Tom De Smet <tom@the-craft.be>
2023-08-02 12:36:25 +0200

    [TASK] Altered original logic for legendary items.
    Legendary items don’t need to get sold. So to reflect this better in the code, we set their sellIn to Infinity which will provide future safety if any other code that alters the sellIn date should be added in the future.
    
    As we overwrite the sellIn for legendary items, we can ditch the decreaseSellIn function and move it back to the core function and apply it to all items.

--- Javascript/src/gilded-tros.js
+++ Javascript/src/gilded-tros.js
@@ -5,10 +5,6 @@ export class GildedTros {
         this.items = items;
     }
     
-    #decreaseSellIn = (item) => {
-        item.sellIn--;
-    };
-    
     #setLimitsToQuality = (quality) => {
         if (quality <= 0) return 0;
         if (quality >= 50) return 50;
@@ -17,24 +13,27 @@ export class GildedTros {
     
     updateQuality() {
         this.items.forEach(item => {
+            /* Day 0 is the day the item was added (we don't know exactly when during the day it was added
+             * so day 0 does not count towards the degrading of the item).
+             * As this function runs at the end of the day, we decrease the sellIn first, so we can work with
+             * the actual remaining days.
+             */
+            item.sellIn--;
+            
             // If sellIn has passed, quality degrades twice as fast.
             let degradeRate = item.sellIn <= 0 ? this.#DEFAULT_DEGRADE_RATE * 2 : this.#DEFAULT_DEGRADE_RATE;
             
             switch (item.name) {
                 case "B-DAWG Keychain":
-                    // Legendary items do not decrease quality nor need to be sold.
+                    /* Legendary items do not decrease quality,
+                     * nor need to be sold, so we keep their sellIn infinitely fresh.
+                     * This will provide future safety if any other code that alters the sellIn date
+                     * should be added in the future.
+                     */
+                    item.sellIn = Infinity;
                     break;
                 case "Backstage passes for Re:Factor":
                 case "Backstage passes for HAXX":
-                    /* Day 0 is the day the item was added (we don't know exactly when during the day it was added
-                     * so day 0 does not count towards the degrading of the item).
-                     * As this function runs at the end of the day, we decrease the sellIn first, so we can work with
-                     * the actual remaining days.
-                     * For this product type, this means the quality of the product needs to be 0 at the end of the day
-                     * as the conference is over by the end and tickets will be useless.
-                     */
-                    this.#decreaseSellIn(item);
-                    
                     // Set default quality rate.
                     let qualityRate = 1;
                     // 'Backstage passes' increases in quality until sell date, so we adjust the quality rate.
@@ -48,21 +47,18 @@ export class GildedTros {
                     item.quality = this.#setLimitsToQuality(itemQuality);
                     break;
                 case "Good Wine":
-                    this.#decreaseSellIn(item);
                     // 'Good wine' increases in quality.
                     if (item.quality < 50) item.quality++;
                     break;
                 case "Duplicate Code":
                 case "Long Methods":
                 case "Ugly Variable Names":
-                    this.#decreaseSellIn(item);
                     // These items degrade in quality twice as fast as normal items.
                     degradeRate *= 2;
                     item.quality -= degradeRate;
                     item.quality = this.#setLimitsToQuality(item.quality);
                     break;
                 default:
-                    this.#decreaseSellIn(item);
                     // Normal items decrease in quality.
                     item.quality -= degradeRate;
                     item.quality = this.#setLimitsToQuality(item.quality);
